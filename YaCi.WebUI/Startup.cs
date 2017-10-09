c8d85acd5921756f5e5ac4a52383c019d9a45d30
Nik <Nik@KOT>
2017-10-09 17:23:42 +0300

    Add project files.

---
+++ YaCi.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YaCi.WebUI.Startup))]
+namespace YaCi.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
